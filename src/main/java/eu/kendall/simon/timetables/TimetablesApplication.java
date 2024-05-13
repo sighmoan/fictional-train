b4ce27235ff3a236a14afba7cbf523ffabb9777f
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-13 09:40:52 +0200

    initial commit

---
+++ src/main/java/eu/kendall/simon/timetables/TimetablesApplication.java
@@ -0,0 +1,13 @@
+package eu.kendall.simon.timetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kendall/simon/timetables/TimetablesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kendall.simon.timetables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
